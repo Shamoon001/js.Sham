613a86cec87e1039162962b4b47acbc325a930dc
Shamoon001 <shamoonzain5@gmail.com>
2025-04-12 15:29:01 +0000

    12 April Work

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "shamoonnzainn5@gmail.com"
+var accountPassword ="12345"
+accountCity = "Karachi"
+
+//accountId = 2//
+accountEmail = "alibitch@gmail.com"
+accountPassword = "Eggburger1222"
+accountCity = "Lahore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
