0028e3ac1d66775bd4b014b208663e2d1749087f
Shamoon001 <shamoonzain5@gmail.com>
2025-04-14 06:27:50 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS Code as newer version
+// alert (3+3) //  we are using node.js , not browser
+
+
+console.log(3
+    +3
+);// readability should be high
+
+console.log("Sham");
+
+let name = "Sham"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// PRIMITIVE DATA TYPES
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Shams");
+console.log(typeof null);// when printing it so the type of null is showing is an object
+console.log(typeof undefined);// type is undefined
+
+
+
--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -4,8 +4,14 @@ var accountPassword ="12345"
 accountCity = "Karachi"
 
 //accountId = 2//
+
 accountEmail = "alibitch@gmail.com"
 accountPassword = "Eggburger1222"
 accountCity = "Lahore"
 console.log(accountId);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
 console.table([accountId,accountEmail,accountPassword,accountCity])
