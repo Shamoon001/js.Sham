d862c5f846367c766951c31e75a0c76972622878
Shamoon001 <shamoonzain5@gmail.com>
2025-04-14 09:57:24 +0000

    comparsionsss

---
+++ 01_Basics/04_comparsions.js
@@ -0,0 +1,28 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(2 ==1)
+//console.log(2 !=1)
+
+
+// console.log("2">1)
+// console.log("02">1);
+
+
+
+// below both the type null and undefined leave it till now it will be more clear laterrr
+
+
+// console.log(null > 0);
+// console.log(null ==0);
+// console.log(null >= 0);
+
+// console.log(undefined ==0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//=== in this it checks the value and its datatype
+
+console.log("2" ===2);
+
+
